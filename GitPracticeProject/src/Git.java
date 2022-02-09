304d2e40fb0a18c654b33ca7bfd9cbc467ff2bca
Khan211 <37129084+Khan211@users.noreply.github.com>
2022-02-08 23:55:40 -0500

    Update Git.java
    These are the changes in repo

--- GitPracticeProject/src/Git.java
+++ GitPracticeProject/src/Git.java
@@ -3,7 +3,12 @@ public class Git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+	
 
 	}
+	
+	public void method(){ 
+		System.out.println("this is changes that I made to this file");
 
 }
